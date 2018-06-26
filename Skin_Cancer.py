470aa30b469f624009730737ef4236514b18fa38
Akhil <40564243+iamakhil6@users.noreply.github.com>
2018-06-26 11:00:30 +0530

    Update Skin_Cancer.py

--- Skin_Cancer.py
+++ Skin_Cancer.py
@@ -4,6 +4,8 @@ from keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout
 import numpy as np
 from keras.preprocessing.image import ImageDataGenerator
 import matplotlib.pyplot as plt
+from sklearn.datasets import load_files
+from keras.utils import np_utils
 
 datagen= ImageDataGenerator(zoom_range=0.1, height_shift_range=0.2, width_shift_range=0.2, rescale=1./255)
 for x_batch in datagen.flow_from_directory('data/train',batch_size=12, target_size=(32,32)):
@@ -14,19 +16,25 @@ for x_batch in datagen.flow_from_directory('data/train',batch_size=12, target_si
         plt.imshow(x_batch[0][i])
     plt.show()
     break
-train_data= datagen.flow_from_directory('data/train', batch_size=30, target_size=(32,32))
-valid_data= datagen.flow_from_directory('data/valid', batch_size=30, target_size=(32,32))
-test_data= datagen.flow_from_directory('data/test', target_size=(32,32))
+train_data= datagen.flow_from_directory('data/train', batch_size=100, target_size=(32,32))
+valid_data= datagen.flow_from_directory('data/valid', batch_size=100, target_size=(32,32))
+# test_data= datagen.flow_from_directory('data/test', target_size=(32,32))
+def load_data(path):
+    data= load_files(path)
+    features= np.array(data['filenames'])
+    labels= np_utils.to_categorical(data['target'], 3)
+    return features, labels
+x_test, y_test= load_data('data/test')
 model= Sequential()
 model.add(Conv2D(filters=8, kernel_size=3, activation='relu', padding='same',input_shape=(32,32,3)))
-model.add(Conv2D(filters=8, kernel_size=3, activation='relu', padding='same'))
+# model.add(Conv2D(filters=8, kernel_size=3, activation='relu', padding='same'))
 model.add(MaxPooling2D(pool_size=2, padding='same'))
 model.add(Conv2D(filters=16, kernel_size=3, activation='relu', padding='same'))
-model.add(Conv2D(filters=16, kernel_size=3, activation='relu', padding='same'))
+# model.add(Conv2D(filters=16, kernel_size=3, activation='relu', padding='same'))
 model.add(MaxPooling2D(pool_size=2, padding='same'))
 model.add(Conv2D(filters=32, kernel_size=3, activation='relu', padding='same'))
-model.add(Conv2D(filters=32, kernel_size=3, activation='relu', padding='same'))
-model.add(Conv2D(filters=32, kernel_size=3, activation='relu', padding='same'))
+# model.add(Conv2D(filters=32, kernel_size=3, activation='relu', padding='same'))
+# model.add(Conv2D(filters=32, kernel_size=3, activation='relu', padding='same'))
 model.add(MaxPooling2D(pool_size=2, padding='same'))
 model.add(Flatten())
 model.add(Dropout(0.4))
@@ -35,13 +43,10 @@ model.add(Dropout(0.4))
 model.add(Dense(3, activation='softmax'))
 model.summary()
 model.compile(loss= 'categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
-best_model= ModelCheckpoint('best_model/skin_cancer.hdf5', save_best_only=True, verbose=1)
+# best_model= ModelCheckpoint('best_model/skin_cancer.hdf5', save_best_only=True, verbose=1)
 # stop= EarlyStopping(monitor='val_loss', patience=2)
-model.fit_generator(train_data, epochs=1, validation_data= valid_data, callbacks=[best_model], shuffle= True)
+model.fit_generator(train_data, epochs=1, validation_data= valid_data, shuffle= True)
 model.load_weights('best_model/skin_cancer.hdf5')
-print(model.evaluate(test_data))
-batch= model.predict(test_data)
-for i in range(12):
-    cell= np.squeeze(batch[i], axis=0)
-    print(cell.shape)
-    plt.imshow(cell)
+predictions = [np.argmax(model.predict(np.expand_dims(feature, axis=0))) for feature in x_test ]
+accuracy= 100* np.sum(np.array(predictions)== np.argmax(y_test, axis=1)/len(predictions))
+print('Accuracy ',accuracy)
